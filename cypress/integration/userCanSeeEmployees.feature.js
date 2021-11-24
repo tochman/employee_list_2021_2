2b6132c96f6a160b13d45b549aee062212f09a82
Thomas <thomas@craftacademy.se>
2021-11-24 10:09:27 +0100

    adds an alias to intercept tests for data type in response

--- cypress/integration/userCanSeeEmployees.feature.js
+++ cypress/integration/userCanSeeEmployees.feature.js
@@ -3,12 +3,16 @@ describe("visiting the application, a user ", () => {
   beforeEach(() => {
     cy.intercept("GET", "https://reqres.in/api/users**", {
       fixture: "usersResponse.json",
-    });
+    }).as("fetchData");
     cy.visit("/");
   });
 
   it("is expected to see a header", () => {
     cy.get("[data-cy=employee-header]").should("contain.text", "Employee List");
+  });
+  
+  it("is expected to return an array of data", () => {
+    cy.wait("@fetchData").its("response.body.data").should("be.an", "array");
   });
 
   it("is expected to see 5 list items", () => {
